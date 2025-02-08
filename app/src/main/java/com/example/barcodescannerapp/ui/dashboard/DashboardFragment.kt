7365896a4e061361a2e82bbaa258e19795aacf70
Kelly Tran <trank@ufl.edu>
2025-02-08 01:18:16 -0500

    Tested reading and displaying the dataset

--- app/src/main/java/com/example/barcodescannerapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/barcodescannerapp/ui/dashboard/DashboardFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.barcodescannerapp.ExcelReader
 import com.example.barcodescannerapp.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -32,9 +34,37 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // Find TextView inside fragment_dashboard.xml and display the excel data
+        val excelDataTextView: TextView = binding.excelDataTextView
+        displayExcelData(excelDataTextView)
+
         return root
     }
 
+    // Read the Excel file and display it in the TextView
+    private fun displayExcelData(textView: TextView) {
+
+        // Create an instance of the ExcelReader class then call the readExcelFile function on it
+        val excelReader = ExcelReader(requireContext())
+        val excelData = excelReader.readExcelFile()
+
+        // Convert each row into a readable string format
+        if (excelData.isNotEmpty()) {
+            val displayText = excelData.joinToString("\n\n") { row ->
+                "• " + row.joinToString(" | ")
+            }
+            // Display the formatted text inside the TextView
+            textView.text = displayText
+
+            /* Testing only first 5 rows:
+            val testData = excelData.take(5)
+            val displayText = testData.joinToString("\n") { row -> row.joinToString(", ") }
+            textView.text = displayText */
+        } else {
+            textView.text = "Error: No data was found."
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
