f80fdc2601c3697dab284a73b6f052dee9a92b86
khushichitalia <kjchitalia@gmail.com>
2025-02-08 09:26:06 -0500

    Search Bar and Item List on Dashboard Page

--- app/src/main/java/com/example/barcodescannerapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/barcodescannerapp/ui/dashboard/DashboardFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.barcodescannerapp.R
+import com.example.barcodescannerapp.databinding.ActivityMainBinding
 import com.example.barcodescannerapp.databinding.FragmentDashboardBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardFragment : Fragment() {
 
@@ -33,6 +40,63 @@ class DashboardFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    // creating variables for listview
+    lateinit var productLV: ListView
+
+    // creating array adapter for listview
+    lateinit var listAdapter: ArrayAdapter<String>
+
+    // creating array list for listview
+    lateinit var productList: ArrayList<String>;
+
+    // creating variable for searchview
+    lateinit var searchView: SearchView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // initializing variables of list view with their ids.
+        productLV = binding.idLVProducts
+        searchView = binding.idSV
+
+        productList = ArrayList()
+        productList.add("La Roche Posay")
+        productList.add("KKW Beauty")
+        productList.add("ELF")
+        productList.add("Revlon")
+        productList.add("Lo'Real")
+
+        // initializing list adapter and setting layout
+        // for each list view item and adding array list to it.
+        listAdapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            productList
+        )
+
+        // on below line setting list
+        // adapter to our list view.
+        productLV.adapter = listAdapter
+
+        // on below line we are adding on query
+        // listener for our search view.
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (productList.contains(query)) {
+                    listAdapter.filter.filter(query)
+                } else {
+                    Toast.makeText(requireContext(), "No Product Found..", Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                listAdapter.filter.filter(newText)
+                return false
+            }
+        })
     }
 
     override fun onDestroyView() {
