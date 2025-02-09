6b0c3be364be5b41d0d6390b9a7d9be8511e82bc
salena-till22 <116910634+salena-till22@users.noreply.github.com>
2025-02-08 21:37:34 -0500

    Updated position of the nav bar to the bottom of the screen and changed the icon titles

--- app/src/main/java/com/example/barcodescannerapp/MainActivity.kt
+++ app/src/main/java/com/example/barcodescannerapp/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = binding.bottomNavigation
         
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
